2d665c02e885ad556c4a4e7a5d3ceb8501c3f67c
FedericoStachurski <rico.stachurski@hotmail.com>
2019-01-26 18:31:03 +0000

    Ephemeris and checking

--- CP_lab.py
+++ CP_lab.py
@@ -5,13 +5,15 @@ from itertools import islice
 import operator
 import math
 import datetime as dt
+import textwrap
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+%matplotlib inline
+
 #%% path file for data
 path="/Users/federicostachurski/Desktop/ASTRO_Lab_PSR-CRAB/data/"
 #%%Read Joerdell Bank observations
 
-jd_data=np.loadtxt(path+"JB_data_1992.cpx")
-print(jd_data[:,0])
-
+jb_data=np.loadtxt(path+"JB_data_1992.cpx")
 
 #%%function for opneing data files
 def openfile_data(filename):
@@ -91,10 +93,6 @@ Signal_P6=signal_plt(data_6,par_6,'6',0)
 Signal_P7=signal_plt(data_7,par_7,'7',0)
 Signal_P8=signal_plt(data_8,par_8,'8',0)
 Signal_P9=signal_plt(data_9,par_9,'9',0)
-
-#%%
-
-
 
 #%%FFT (function), takes data col 1 and 2, makes complex num with 
 #data, runs complex data in FFT, returns FFT, plts FFT of Signals
@@ -209,10 +207,55 @@ def jd_to_date(jd):
         year = D - 4716
     else:
         year = D - 4715
-    return year, month, day
+    frac_day = day - int(day)
+    hours = frac_day * 24.
+    hours, hour = math.modf(hours)
+    mins = hours * 60.
+    mins, min = math.modf(mins)
+    secs = mins * 60.
+    secs, sec = math.modf(secs)
+    micro = round(secs * 1.e6)
+    return year, month, int(day), int(hour), int(min), int(sec)
 
+# print(jd_to_date(par_1[0]))
+# print(jd_to_date(par_2[0]))
+# print(jd_to_date(par_3[0]))
+# print(jd_to_date(par_4[0]))
+# print(jd_to_date(par_5[0]))
+# print(jd_to_date(par_6[0]))
+# print(jd_to_date(par_7[0]))
+# print(jd_to_date(par_8[0]))
+# print(jd_to_date(par_9[0]))
+#%%
+date_1=list(jd_to_date(par_1[0]))
+del date_1[-1]
+date_2=list(jd_to_date(par_2[0]))
+del date_2[-1]
+date_3=list(jd_to_date(par_3[0]))
+del date_3[-1]
+date_4=list(jd_to_date(par_4[0]))
+del date_4[-1]
+date_5=list(jd_to_date(par_5[0]))
+del date_5[-1]
+date_6=list(jd_to_date(par_6[0]))
+del date_6[-1]
+date_7=list(jd_to_date(par_7[0]))
+del date_7[-1]
+date_8=list(jd_to_date(par_8[0]))
+del date_8[-1]
+date_9=list(jd_to_date(par_9[0]))
+del date_9[-1]
+#%%
+print(date_1)
+print(date_2)
+print(date_3)
+print(date_4)
+print(date_5)
+print(date_6)
+print(date_7)
+print(date_8)
+print(date_9)
 
-print(jd_to_date(par_1[0]))
 
 #%% change mjd into jd
 def jd_to_mjd(jd):
@@ -220,23 +263,25 @@ def jd_to_mjd(jd):
 
 
 #%% Frequency plot
-plt.plot(jd_to_mjd(Jdate_array),Freq_array,'ro-')
+plt.plot(jd_to_mjd(Jdate_array),Freq_array*1.0001,'ro')
 plt.grid(True)
-slope, intercept = np.polyfit(jd_to_mjd(Jdate_array), Freq_array, 1)
+slope, intercept = np.polyfit(jd_to_mjd(Jdate_array), Freq_array*1.0001, 1)
+plt.plot(jd_to_mjd(Jdate_array),(slope*jd_to_mjd(Jdate_array))+intercept, 'g')
 ax = plt.gca()
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
-ax.errorbar(jd_to_mjd(Jdate_array), Freq_array, 1/(4096*10.24),fmt='ro')
+ax.errorbar(jd_to_mjd(Jdate_array), Freq_array*1.0001, 1/(4096*10.24),fmt='ro')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('MJD')
 plt.title('Frquency Slow down')
 plt.show()
+# print(jd_to_date(Jdate_array))
 
 print(slope)
 #%% Jordell Bank data plot
-plt.plot(jd_data[:,1],jd_data[:,0],'bo-')
+plt.plot(jb_data[:,1],jb_data[:,0],'bo-')
 ax = plt.gca()
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
-slope_jb, intercept_jb = np.polyfit(jd_data[:,1], jd_data[:,0], 1)
+slope_jb, intercept_jb = np.polyfit(jb_data[:,1], jb_data[:,0], 1)
 plt.grid(True)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('MJD')
@@ -245,23 +290,144 @@ plt.show()
 print(slope_jb)
 
 #%% One plot
-
-plt.plot(jd_to_mjd(Jdate_array),Freq_array,'ro-')
-plt.grid(True)
-slope, intercept = np.polyfit(jd_to_mjd(Jdate_array), Freq_array, 1)
-plt.plot(jd_data[:,1],jd_data[:,0],'bo-')
+plt.plot(jd_to_mjd(Jdate_array),Freq_array*1.0001,'ro')
 ax = plt.gca()
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+ax.errorbar(jd_to_mjd(Jdate_array), Freq_array*1.0001, 1/(4096*10.24),fmt='ro')
+plt.grid(True)
+slope, intercept = np.polyfit(jd_to_mjd(Jdate_array), Freq_array*1.0001, 1)
+plt.plot(jd_to_mjd(Jdate_array),(slope*jd_to_mjd(Jdate_array))+intercept, 'g')
+plt.plot(jb_data[:,1],jb_data[:,0],'bo-')
+ax = plt.gca()
+ax.get_yaxis().get_major_formatter().set_useOffset(False)
+textstr = '\n'.join((
+r"Slope JB = "+str(float(slope_jb)),
+r"Intercept JB = "+str(float(intercept_jb)),
+r"Slope Data = " +str(float(slope)),
+r"Intercept Data = " +str(float(intercept))))
+plt.text(0.8,0.4, textstr,fontsize=11,horizontalalignment='left',transform=ax.transAxes,bbox=dict(facecolor='white', alpha=1))
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('MJD')
 plt.title('Frquency Slow down')
 
+#%% Pulsar timing corrections
+#read Earth-Sun Ephemeris from 1992/3/4 - 1992/3/17
+def read_ephe(c):
+    ephe=open(path+"horizons_result_3.txt", "r")
+    lines = ephe.readlines()
+    column = []
+    i = 0
+    for line in lines:
+        newArr = (line.split())
+        for x in newArr:
+            if len(x) < 3:
+                newArr.remove(x)
+        column.append(float(newArr[c]))
+    return column
+    ephe.close()
+jdate_ephe=read_ephe(0)
+RA_ephe=read_ephe(1)
+DEC_ephe=read_ephe(2)
+Vobs_ephe=read_ephe(4)
+EcLON_ephe=read_ephe(5)
+EcLAT_ephe=read_ephe(6)
+
+#%%convert Jdate to Standard Date 
+date_eph=list(map(jd_to_date,jdate_ephe))
+date_eph=list(map(list, date_eph))
+print(date_eph[-1])
+
+#%% remove seconds
+i=0
+while i<len(date_eph):
+    if date_eph[i][5] == 59 :
+        date_eph[i][4] = date_eph[i][4] + 1
+    del date_eph[i][-1]
+    i=i+1
+print(date_eph[770])
+#%%check position of dates in ephemeris
+
+i=0
+while i<len(date_eph) :
+    if date_eph[i] == [1992, 3, 5, 12, 46]:
+        print(i)
+    i=i+1
+
+#%%
 
 
-#%% METHOD 2: look at entire data
 
-tot_data=np.concatenate([data_1,data_2,data_3,data_4,data_5,data_6,data_7,data_8,data_9],axis=0)
-print(tot_data)
-#tot_signal=signal_plt(tot_data,par_1,'1 2 3 4',0)
-tot_FFT=FastFT(tot_data,par_1,0)
+k=0
+pos=np.zeros(7)
+while k<=6:
+    j=0
+    while j<len(date_eph):
+        if date_eph[j] == vars()['date_'+str(k+3)]:
+            pos[k]=j
+        j=j+1
+    k=k+1    
 
+print(pos)
+
+
+
+
+
+
+
+
+
+
+
+#%% likelyhood functions
+
+err = 1/(4096*10.24)
+Freq_corr = Freq_array*1.0001
+epoch = 3 # the index of the epoch of the solution
+t = (jd_to_mjd(Jdate_array) - jd_to_mjd(Jdate_array[epoch]))*24*3600
+best_f= np.polyfit(t-t[epoch],Freq_corr,1)
+f_funct = np.poly1d(best_f)
+# print(best_f)
+fdot=best_f[0]
+f_mu=best_f[1]
+
+def logL1(fdot,f): # the log likelihood as a function of fdot and f
+    sum = 0
+    for i in range(0,7):
+        sum += (Freq_corr[i] - (fdot*t[i] + f))**2/err**2
+    return -sum/2
+
+#%% plot likelyhood
+#frequency search box size
+f_width = err*2
+#fdot search box size
+fdot_width = err*2/t[-1]
+# box centre
+f_cent = f_funct(0)
+fdot_cent = (f_funct(500)-f_funct(-500))/(1000)
+boxres=200 #box resolution
+f_vals = f_cent + np.linspace(-f_width , f_width, boxres)
+fdot_vals = fdot_cent + np.linspace(-fdot_width, fdot_width, boxres)
+X,Y = np.meshgrid(fdot_vals, f_vals)
+Z = logL1(X, Y)
+prob = np.exp(Z-Z.max())
+fig, ax = plt.subplots()
+ax.get_yaxis().get_major_formatter().set_useOffset(False)
+p = ax.pcolor(X, Y, prob, cmap='jet', vmin=prob.min(), vmax=prob.max())
+cm = ax.contour(X, Y, prob, np.arange(0,1,0.2),alpha=1,linewidths=2)
+ax.clabel(cm, inline=True)
+cb = fig.colorbar(p)
+plt.xlabel('fdot (Hz/s)')
+plt.ylabel('frequency (Hz)')
+plt.show()
+
+#%%
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(1,1,1, projection='3d')
+ax.get_yaxis().get_major_formatter().set_useOffset(False)
+ax.plot_surface(X, Y, prob, alpha=0.4)
+cset = ax.contour(X, Y, prob, zdir='z', cmap='jet')
+ax.view_init(40, 30)
+plt.show()
+# cset = ax.contour(X, Y, Z, zdir='x', cmap='jet')
+# cset = ax.contour(X, Y, Z, zdir='y', cmap='jet')
